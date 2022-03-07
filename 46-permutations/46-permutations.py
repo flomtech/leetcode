381d7f3a3475e5acc62fa9b7b91ef671ba6327fb
Evans Meja <evanslagat911@gmail.com>
2022-03-07 21:28:19 +0300

    Time: 70 ms (5.32%), Space: 13.4 MB (86.61%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,23 @@
+
+
+from math import factorial
+
+class Solution(object):
+
+    def calculate_perm(self,my_l):
+        permutations=[]
+        l=len(my_l)
+        for x in xrange(factorial(l)):
+            av=list(my_l)
+            np=[]
+            for radix in xrange(l, 0, -1):
+                pv=factorial(radix-1)
+                index=x/pv
+                np.append(av.pop(index))
+                x-=index*pv
+            permutations.append(np)
+        return permutations
+
+    def permute(self, nums):
+        return self.calculate_perm(nums)
+        
